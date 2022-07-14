90e052535c3c24a096c570499367424af12273b2
jaypath <69011236+jaypath@users.noreply.github.com>
2022-07-14 12:26:09 -0400

    Update linkReport.jl

--- JuliaFiles/src/linkReport.jl
+++ JuliaFiles/src/linkReport.jl
@@ -119,6 +119,3 @@ function matchReport2Metadata(reports,metareports)
   return metareports
 end
 
-ppr = DataFrame(CSV.File("ppr.csv"));
-ppr_matched = matchReport2Metadata(reports,ppr);
-ppr_matched = matchReport2Recording(ppr_matched,augmented_signals);
