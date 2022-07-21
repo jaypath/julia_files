cfdf70a9157456c540cadd50225289becea6eae5
jaypath <69011236+jaypath@users.noreply.github.com>
2022-07-21 13:39:56 -0400

    Update exportAWSData.jl

--- JuliaFiles/src/exportAWSData.jl
+++ JuliaFiles/src/exportAWSData.jl
@@ -1,4 +1,4 @@
-# 7/21/22 From Michelle
+# 7/21/22 From Michelle and Eric
 
 using AWSS3
 using BOME
@@ -6,5 +6,9 @@ using DataFrames
 using Legolas
 
 save_path = S3Path("s3://project-pr-001-sandbox/new.bome.annotation.arrow")
-Legolas.write(save_path, df, Legolas.Schema("bome.annotation@1")) #df is a dataframe or table
 
+#if legolas schema
+#Legolas.write(save_path, df, Legolas.Schema("bome.annotation@1")) #df is a dataframe or table
+
+#write back to S3, dataframe only
+Arrow.write(path, myDataFrame)
