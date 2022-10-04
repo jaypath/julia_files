d697ebcfdc25badde03d83a4f59bb8869184a60c
jaypath <69011236+jaypath@users.noreply.github.com>
2022-10-04 18:43:37 -0400

    Update BMS_demographicsPull.jl

--- BMSScripts/BMS_demographicsPull.jl
+++ BMSScripts/BMS_demographicsPull.jl
@@ -21,8 +21,8 @@ AD_data = dropmissing(unique(recordsWithDXandICD("",unique(AD,:code).code),:reco
 AD_data = subset(AD_data,:age_in_years=>ByRow(a->a>ageMin && a<ageMax);skipmissing=true); #by recording, and min age
 
 #exclude 
-exclusionDXs = ["vascular dementia","stroke","alcohol","substance","encephalitis","Creutzfeldt","multiple sclerosis","brain tumor","neoplasm of brain","schizophrenia","glioblastoma","glioma","astrocytoma","cirrhosis","renal failure"];
-exclusionICDs = ["C71.9","C71.90","C71.91","C71.92","191.9"]; #brain tumor, nos codes
+exclusionDXs = ["CNS lymphoma","cocaine dependence","cocaine use","opiate dependence","opiate use","Pick's disease","fronto-temporal dementia","frontotemporal dementia","hydrocephalus","lewy body","multiple systems atrophy","ALS","amyotrophic lateral sclerosis","encephalomalacia","muscular dystrophe","trisomy","mitochondrial","vascular dementia","stroke","alcohol","substance","encephalitis","Creutzfeldt","multiple sclerosis","brain tumor","neoplasm of brain","schizophrenia","glioblastoma","glioma","astrocytoma","cirrhosis","renal failure"];
+exclusionICDs = ["C71.9","C71.90","C71.91","C71.92","191.9","192.1","198.3","200.50",]; #brain tumor, nos codes
 AD_x = sort(select(unique(listDXandICD(exclusionDXs,exclusionICDs,AD_data),[:diagnosis,:code]),:code,:diagnosis),:code);
 CSV.write("ICD_exclude.csv",AD_x); #review this CSV!
 
@@ -42,6 +42,10 @@ AD_EEG = testType(AD_data,"EEG");
 AD_EEG_bin = binThisDF(AD_EEG,:subject)
 CSV.write("EEGbin.csv",AD_EEG_bin); #review this CSV!
 
-AD_EEG_ICD = sort(select(unique(searchRecICD(AD_EEG),:diagnosis),:code,:diagnosis),:code);
-CSV.write("EEGICD.csv",AD_EEG_ICD); #review this CSV!
+AD_EEG_ICD = sort(select(unique(searchRecICD(AD_EEG),[:diagnosis,:code,:subject]),:code,:diagnosis,:subject),:code);
+CSV.write("EEGICD.csv",sort(unique(select(AD_EEG_ICD,:code,:diagnosis),[:diagnosis,:code]),:code)); #review this CSV!
+
+AD_EEG_ICD_bin = sort(binThisDF(unique(AD_EEG_ICD,[:code, :subject]),:code),:code);
+CSV.write("EEGICD_bysubject.csv",AD_EEG_ICD_bin); #review this CSV!
+
 
